e2ce9fe331ed20e75b0b843f65d05036397c6baf
Wright Rickman <wrightrickman@gmail.com>
2014-02-26 20:59:30 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OperationUmbra::Application.config.secret_key_base = '165d4ecbf1e0142dc29e01898c088122f9f0f81dc05184e31077440782bdbbcb3cdd93abf002d2ba80e76b3672e00458585d43ecbe81cccaf1e515e5045865fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OperationUmbra::Application.config.session_store :cookie_store, key: '_operation_umbra_session'
