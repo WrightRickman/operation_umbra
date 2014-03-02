38e0e678d20cb6c59bf90b1bb28b9904db23ebec
Wright Rickman <wrightrickman@gmail.com>
2014-03-01 19:27:19 -0500

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,6 @@ Game.destroy_all
 
 yaniv = User.create(:user_name => "Yaniv", :email => "yaniv@email.com", :password => "password123")
 Game.create(:name => "The Tenth Plaguers", :max_difficulty => 3, :creator_id => yaniv.id)
+
+wright = User.create(:user_name => "Wright", :email => "wright@email.com", :password => "password456")
+Game.create(:name => "Operation Azorius", :max_difficulty => 3, :creator_id => wright.id)
